72bde8a07cf7ac3915765a7fbc40a688f4bccde9
Colin Childs <colinchilds@gmail.com>
2025-02-12 18:12:22 -0700

    Add back button. Add some more styling

--- web/App.jsx
+++ web/App.jsx
@@ -54,6 +54,10 @@ function App() {
       });
   };
 
+  const goBack = () => {
+    setIsGenerating(false);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsGenerating(true);
@@ -83,88 +87,97 @@ function App() {
 
   return (
     <div style={{"background-image": `url(${bgUrl})`}} class="bg-cover bg-gray-500 flex items-center justify-center min-h-screen">
-        <form
-          onSubmit={handleSubmit}
-          id="blueprint_form"
-          classList={{hidden: isGenerating()}}
-          class="bg-gray-500 gray-box-border p-6 rounded shadow-md w-full max-w-md">
-            <div class="mb-4">
-                <label class="block text-tan-500 text-sm font-bold mb-2" for="fileInput">Select File</label>
-                <input
-                    class="w-full px-3 py-2 border rounded focus:outline-none focus:ring"
-                    type="file"
-                    id="gifInput"
-                    name="gifInput"
-                    required
-                />
-            </div>
-            <div class="mb-4">
-                <label class="block text-tan-500 text-sm font-bold mb-2" for="framerate">Framerate</label>
-                  <input
+        <div classList={{hidden: isGenerating()}} class="panel">
+            <h2 class="text-tan-500">Convert GIF to Blueprint</h2>
+            <form
+              onSubmit={handleSubmit}
+              id="blueprint_form"
+              class="panel-inset bg-gray-500 p-6 rounded shadow-md w-full max-w-md">
+                <div class="mb-4">
+                    <label class="block text-tan-500 text-sm font-bold mb-2" for="fileInput">Select File</label>
+                    <input
+                        class="bg-very-light-gray-500 w-full px-3 py-2 border rounded focus:outline-none focus:ring"
+                        type="file"
+                        id="gifInput"
+                        name="gifInput"
+                        required
+                    />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-tan-500 text-sm font-bold mb-2" for="framerate">Framerate</label>
+                      <input
+                            class="bg-very-light-gray-500 focus:bg-tan-500 w-full px-3 py-2 border rounded focus:outline-none focus:ring"
+                            type="number"
+                            id="framerate"
+                            name="framerate"
+                            placeholder="Enter max framerate (won't exceed original)"
+                            value="15"
+                      />
+                </div>
+                <div class="mb-4">
+                    <label class="block text-tan-500 text-sm font-bold mb-2" for="maxwidth">Max Width</label>
+                    <input
                         class="bg-very-light-gray-500 focus:bg-tan-500 w-full px-3 py-2 border rounded focus:outline-none focus:ring"
                         type="number"
-                        id="framerate"
-                        name="framerate"
-                        placeholder="Enter max framerate (won't exceed original)"
-                        value="30"
-                  />
-            </div>
-            <div class="mb-4">
-                <label class="block text-tan-500 text-sm font-bold mb-2" for="maxwidth">Max Width</label>
-                <input
-                    class="bg-very-light-gray-500 focus:bg-tan-500 w-full px-3 py-2 border rounded focus:outline-none focus:ring"
-                    type="number"
-                    id="maxsize"
-                    name="maxsize"
-                    placeholder="Enter max width"
-                    value="100"
-                />
-            </div>
-            <div class="mb-4 flex items-center">
-                <span class="mr-4 text-tan-500 font-bold">Space Age?</span>
-                <label for="toggle" class="flex items-center cursor-pointer">
-                    <div class="relative">
-                        <input ref={toggleInput} onChange={toggleChange} id="toggle" type="checkbox" name="toggle" class="sr-only" />
-                        <div ref={toggleBg} id="togglebg" class="bg-very-light-gray-500 block w-14 h-8 rounded-full"></div>
-                        <div ref={dot} class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition-transform"></div>
-                    </div>
-                    <div ref={toggleLabel} class="ml-3 text-tan-500 font-medium" id="toggleLabel">No</div>
-                </label>
-            </div>
-            <div id="quality" class="mb-4" classList={{hidden: !isDlc()}}>
-                <label class="block text-tan-500 text-sm font-bold mb-2" for="substationQuality">Substation Quality</label>
-                <select ref={substationQualityDiv} id="substationQuality" name="substationQuality" class="bg-very-light-gray-500 w-full px-3 py-2 border rounded focus:outline-none focus:ring">
-                    <option value="normal">Normal</option>
-                    <option value="uncommon">Uncommon</option>
-                    <option value="rare">Rare</option>
-                    <option value="epic">Epic</option>
-                    <option value="legendary">Legendary</option>
-                </select>
-            </div>
-            <div class="flex items-center justify-between">
-                <button
-                    class="bg-green-500 hover:bg-bright-green-500 text-black font-bold py-2 px-4"
-                    ref={submitButton}
-                    id="submit"
-                    type="submit">
-                    Submit
-                </button>
-            </div>
-        </form>
-
-        <div ref={blueprintStatus} id="blueprintStatus" class="bg-white p-6 rounded shadow-md w-full max-w-md" classList={{hidden: !isGenerating()}}>
-            <h2 ref={blueprintTitle} id="blueprintTitle" class="text-xl font-bold mb-2"></h2>
-            <div ref={progressContainer} id="progressContainer">
-                <div class="w-full bg-gray-200 rounded-full h-4 mb-2">
-                    <div ref={progressBar} id="progressBar" class="bg-blue-500 h-4 rounded-full" style="width: 0%"></div>
+                        id="maxsize"
+                        name="maxsize"
+                        placeholder="Enter max width"
+                        value="50"
+                        min="2"
+                        max="300"
+                    />
                 </div>
-                <p ref={progressStatus} id="progressStatus" class="text-gray-700">Starting...</p>
+                <div class="mb-4 flex items-center">
+                    <span class="mr-4 text-tan-500 font-bold">Space Age?</span>
+                    <label for="toggle" class="flex items-center cursor-pointer">
+                        <div class="relative">
+                            <input ref={toggleInput} onChange={toggleChange} id="toggle" type="checkbox" name="toggle" class="sr-only" />
+                            <div ref={toggleBg} id="togglebg" class="bg-very-light-gray-500 block w-14 h-8 rounded-full"></div>
+                            <div ref={dot} class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition-transform"></div>
+                        </div>
+                        <div ref={toggleLabel} class="ml-3 text-tan-500 font-medium" id="toggleLabel">No</div>
+                    </label>
+                </div>
+                <div id="quality" class="mb-4" classList={{hidden: !isDlc()}}>
+                    <label class="block text-tan-500 text-sm font-bold mb-2" for="substationQuality">Substation Quality</label>
+                    <select ref={substationQualityDiv} id="substationQuality" name="substationQuality" class="bg-very-light-gray-500 w-full px-3 py-2 border rounded focus:outline-none focus:ring">
+                        <option value="normal">Normal</option>
+                        <option value="uncommon">Uncommon</option>
+                        <option value="rare">Rare</option>
+                        <option value="epic">Epic</option>
+                        <option value="legendary">Legendary</option>
+                    </select>
+                </div>
+                <div class="flex items-center justify-end">
+                    <button
+                        class="button button-green-right"
+                        ref={submitButton}
+                        id="submit"
+                        type="submit">
+                        Generate
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div ref={blueprintStatus} id="blueprintStatus" class="panel p-6 rounded shadow-md w-full max-w-md" classList={{hidden: !isGenerating()}}>
+            <h2 ref={blueprintTitle} id="blueprintTitle" class="text-tan-500 text-xl font-bold mb-2"></h2>
+            <div ref={progressContainer} id="progressContainer">
+                <div class="w-full bg-dark-gray-500 rounded-full h-4 mb-2">
+                    <div ref={progressBar} id="progressBar" class="bg-green-500 h-4 rounded-full" style="width: 0%"></div>
+                </div>
+                <p ref={progressStatus} id="progressStatus" class="text-tan-500">Starting...</p>
             </div>
             <div ref={blueprintResult} id="blueprintResult" classList={{hidden: !isGenerating()}}>
-                <div ref={responseText} id="responseText" class="bg-gray-50 p-4 border rounded overflow-auto max-h-64 mb-2"></div>
-                <button onClick={toClipboard} id="copyButton" class="bg-green-500 hover:bg-bright-green-500 text-white font-bold py-1 px-2 rounded">
-                    Copy
-                </button>
+                <div ref={responseText} id="responseText" class="panel-inset-light text-very-light-gray-500 p-3 border rounded overflow-auto max-h-64 mb-6"></div>
+                <div class="flex items-center justify-between">
+                  <button onClick={goBack} id="backButton" class="button">
+                      Back
+                  </button>
+                  <button onClick={toClipboard} id="copyButton" class="button button-green">
+                      Copy
+                  </button>
+                </div>
             </div>
         </div>
     </div>
