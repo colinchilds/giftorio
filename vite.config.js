f2bb95519f5d09cdf69b25f920d0dc37e068e240
Colin Childs <colinchilds@gmail.com>
2025-02-19 17:39:51 -0700

    Add some basic SEO

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.gif', '**/*.jpeg'],
+    plugins: [
+        solidPlugin(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'robots.txt',
+                    dest: '.'
+                }
+            ]
+        })
+    ],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: 'esnext',
+    },
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.gif', '**/*.jpeg'],
 });
